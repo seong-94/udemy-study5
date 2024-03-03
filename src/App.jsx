1524c21e1da8d5e433d468987561190f98e7357f
seong-94 <togussla@naver.com>
2024-03-04 00:10:25 +0900

    강의 171

--- src/App.jsx
+++ src/App.jsx
@@ -66,11 +66,12 @@ function App() {
   const ctxValue = {
     items: shoppingCart.items,
     onAddItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext.Provider value={ctxValue}>
-      <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
